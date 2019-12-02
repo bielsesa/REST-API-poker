709b30c28ce7de042887518533789ecae60c2cfd
Biel Serrano Sanchez <bielsesa@gmail.com>
2019-12-02 11:12:30 +0100

    Arxiu renombrat, implementació diverses partides

--- api_rest.js
+++ api_rest.js
@@ -7,27 +7,43 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/*let partides = [];
-let cartesPartida = [];
-let primerJugador = [];
-let segonJugador = [];*/
+let partides = [];
+//let jugadors = [];
 
-let partida = { codi: -1 };
+//let partida = { codi: -1 };
 let primerJugador = { cartes: [], aposta: 0 };
 let segonJugador = { cartes: [], aposta: 0 };
 
 /*************** S'INICIA LA PARTIDA ***************/
 app.post("/iniciarJoc/:codiPartida", (req, res) => {
   console.log("POST /iniciarJoc/codiPartida");
-  partida.codi = req.body.codiPartida;
-  console.log(`Creada partida amb codi: ${partida.codi}`);
-  res.send(`Has entrat a la sala número ${partida.codi}.`);
+
+  partides.push({
+    codi: req.body.codiPartida,
+    primerJugador: { cartes: [], aposta: 0 },
+    segonJugador: { cartes: [], aposta: 0 }
+  });
+
+  //partida.codi = req.body.codiPartida;
+  console.log(`S'ha creat una partida amb codi: ${req.body.codiPartida}`);
+  console.log("Partides existents:\n");
+  partides.forEach(p => console.log(`${p.codi}`));
+  res.send(`Has entrat a la sala número ${req.body.codiPartida}.`);
 });
 
 /*************** ES REPARTEIXEN LES CARTES ALS JUGADORS ***************/
 app.get("/obtenirCarta/:codiPartida", (req, res) => {
+  let partidaInput = { codiPartida: req.params.codiPartida };
+
+  let trobaPartida = p => {
+    if (p.codi == partidaInput.codiPartida) return p;
+  };
+
+  let partida = partides.find(trobaPartida);
+
   // comprova que la partida existeix
-  if (req.params.codiPartida != partida.codi) {
+  //if (req.params.codiPartida != partida.codi) {
+  if (partida == undefined) {
     res.send(`Aquesta partida no existeix.`);
   } else {
     // llegeix la baralla de cartes de l'arxiu JSON
@@ -47,39 +63,39 @@ app.get("/obtenirCarta/:codiPartida", (req, res) => {
         let numCarta;
 
         // primer jugador
-        while (primerJugador.cartes.length != 5) {
+        while (partida.primerJugador.cartes.length != 5) {
           numCarta = Math.floor(Math.random() * 52); // selecciona una carta al atzar, del 1 al 52
 
           // comprova que la carta no estigui ja repartida
           if (!cartesRepartides.includes(numCarta)) {
             cartesRepartides.push(numCarta);
-            primerJugador.cartes.push(data.baralla[numCarta]);
+            partida.primerJugador.cartes.push(data.baralla[numCarta]);
           }
         }
 
         /******* DEBUG *******/
 
         console.log("Cartes jugador 1:");
-        primerJugador.cartes.forEach(carta => {
+        partida.primerJugador.cartes.forEach(carta => {
           console.log(`${carta.pal}, ${carta.color}, ${carta.carta}`);
         });
         /******* DEBUG *******/
 
         // segon jugador
-        while (segonJugador.cartes.length != 5) {
+        while (partida.segonJugador.cartes.length != 5) {
           numCarta = Math.floor(Math.random() * 52); // selecciona una carta al atzar, del 1 al 52
 
           // comprova que la carta no estigui ja repartida
           if (!cartesRepartides.includes(numCarta)) {
             cartesRepartides.push(numCarta);
-            segonJugador.cartes.push(data.baralla[numCarta]);
+            partida.segonJugador.cartes.push(data.baralla[numCarta]);
           }
         }
 
         /******* DEBUG *******/
 
         console.log("Cartes jugador 2:");
-        segonJugador.cartes.forEach(carta => {
+        partida.segonJugador.cartes.forEach(carta => {
           console.log(`${carta.pal}, ${carta.color}, ${carta.carta}`);
         });
         /******* DEBUG *******/
@@ -96,20 +112,39 @@ app.get("/obtenirCarta/:codiPartida", (req, res) => {
 
 /*************** ES MOSTREN LES CARTES DELS DOS JUGADORS ***************/
 app.get("/mostrarCartes/:codiPartida", (req, res) => {
-  if (req.params.codiPartida != partida.codi) {
-    res.send(`Aquesta partida no existeix.`);
-    return;
-  }
+  let partidaInput = { codiPartida: req.params.codiPartida };
 
-  res.send(`Cartes primer jugador: 
-            ${JSON.stringify(primerJugador.cartes)} 
+  let trobaPartida = p => {
+    if (p.codi == partidaInput.codiPartida) return p;
+  };
+
+  let partida = partides.find(trobaPartida);
+
+  // comprova que la partida existeix
+  //if (req.params.codiPartida != partida.codi) {
+  if (partida == undefined) {
+    res.send(`Aquesta partida no existeix.`);
+  } else {
+    res.send(`Cartes primer jugador: 
+            ${JSON.stringify(partida.primerJugador.cartes)} 
             Cartes segon jugador: 
-            ${JSON.stringify(segonJugador.cartes)}`);
+            ${JSON.stringify(partida.segonJugador.cartes)}`);
+  }
 });
 
 /*************** EL JUGADOR LLENÇA UNA CARTA ***************/
 app.put("/tirarCarta/codiPartida/carta", (req, res) => {
-  if (req.body.codiPartida != partida.codi) {
+  let partidaInput = { codiPartida: req.body.codiPartida };
+
+  let trobaPartida = p => {
+    if (p.codi == partidaInput.codiPartida) return p;
+  };
+
+  let partida = partides.find(trobaPartida);
+
+  // comprova que la partida existeix
+  //if (req.body.codiPartida != partida.codi) {
+  if (partida == undefined) {
     res.send(`Aquesta partida no existeix.`);
   } else {
     carta = {
@@ -127,12 +162,12 @@ app.put("/tirarCarta/codiPartida/carta", (req, res) => {
         return c;
     };
 
-    console.log(`\nS'ha intentar llençar la carta: ${JSON.stringify(carta)}`);
+    console.log(`\nS'ha intentat llençar la carta: ${JSON.stringify(carta)}`);
 
     if (req.body.codiJugador == 0) {
-      if (primerJugador.cartes.find(trobaCarta) != undefined) {
-        primerJugador.cartes.splice(
-          primerJugador.cartes.findIndex(trobaCarta),
+      if (partida.primerJugador.cartes.find(trobaCarta) != undefined) {
+        partida.primerJugador.cartes.splice(
+          partida.primerJugador.cartes.findIndex(trobaCarta),
           1
         );
         res.send(
@@ -142,9 +177,9 @@ app.put("/tirarCarta/codiPartida/carta", (req, res) => {
         res.send("El primer jugador no té aquesta carta.");
       }
     } else if (req.body.codiJugador == 1) {
-      if (segonJugador.cartes.find(trobaCarta) != undefined) {
-        segonJugador.cartes.splice(
-          segonJugador.cartes.findIndex(trobaCarta),
+      if (partida.segonJugador.cartes.find(trobaCarta) != undefined) {
+        partida.segonJugador.cartes.splice(
+          partida.segonJugador.cartes.findIndex(trobaCarta),
           1
         );
         res.send(
@@ -153,45 +188,86 @@ app.put("/tirarCarta/codiPartida/carta", (req, res) => {
       } else {
         res.send("El segon jugador no té aquesta carta.");
       }
+    } else {
+      res.send(`Aquest jugador no existeix.`);
     }
   }
 });
 
 /*************** EL JUGADOR APOSTA UNA QUANTITAT ***************/
 app.put("/moureJugador/codiPartida/aposta/quantitat", (req, res) => {
-  if (req.body.codiPartida != partida.codi) {
+  let partidaInput = { codiPartida: req.body.codiPartida };
+
+  let trobaPartida = p => {
+    if (p.codi == partidaInput.codiPartida) return p;
+  };
+
+  let partida = partides.find(trobaPartida);
+
+  // comprova que la partida existeix
+  //if (req.body.codiPartida != partida.codi) {
+  if (partida == undefined) {
     res.send(`Aquesta partida no existeix.`);
   } else {
     if (req.body.codiJugador == 0) {
-      primerJugador.aposta = req.body.quantitatAposta;
-      res.send(`El primer jugador ha apostat:  ${primerJugador.aposta}`);
+      partida.primerJugador.aposta = req.body.quantitatAposta;
+      console.log(
+        `El primer jugador ha apostat:  ${req.body.quantitatAposta}€`
+      );
+      res.send(
+        `El primer jugador ha apostat:  ${partida.primerJugador.aposta}€`
+      );
     } else if (req.body.codiJugador == 1) {
-      segonJugador.aposta = req.body.quantitatAposta;
-      res.send(`El primer jugador ha apostat:  ${segonJugador.aposta}`);
+      partida.segonJugador.aposta = req.body.quantitatAposta;
+      console.log(`El segon jugador ha apostat:  ${req.body.quantitatAposta}€`);
+      res.send(`El segon jugador ha apostat:  ${partida.segonJugador.aposta}€`);
+    } else {
+      res.send(`Aquest jugador no existeix.`);
     }
   }
 });
 
 /*************** EL JUGADOR PASSA ***************/
 app.put("/moureJugador/codiPartida/passa", (req, res) => {
-  if (req.body.codiPartida != partida.codi) {
+  let partidaInput = { codiPartida: req.body.codiPartida };
+
+  let trobaPartida = p => {
+    if (p.codi == partidaInput.codiPartida) return p;
+  };
+
+  // comprova que la partida existeix
+  //if (req.body.codiPartida != partida.codi) {
+  if (partides.find(trobaPartida) == undefined) {
     res.send(`Aquesta partida no existeix.`);
   } else {
     if (req.body.codiJugador == 0) {
       res.send(`El primer jugador passa.`);
     } else if (req.body.codiJugador == 1) {
       res.send(`El segon jugador passa.`);
+    } else {
+      res.send(`Aquest jugador no existeix.`);
     }
   }
 });
 
-app.delete("/acabarJoc/:codi", (req, res) => {
-  let partida = partides.find(codi => codi === req.params.codi);
+app.delete("/acabarJoc/:codiPartida", (req, res) => {
+  let partidaInput = { codiPartida: req.params.codiPartida };
+
+  let trobaPartida = p => {
+    if (p.codi == partidaInput.codiPartida) return p;
+  };
+
+  let partida = partides.find(trobaPartida);
+
+  console.log(`Partida: ${JSON.stringify(partida)}`);
+
+  // comprova que la partida existeix
+  //if (req.params.codiPartida != partida.codi) {
   if (partida == undefined) {
     res.send(`Aquesta partida no existeix.`);
   } else {
     partides.splice(partides.indexOf(partida), 1);
-    res.send("Partida acabada.");
+    res.send(`S'ha acabat la partida de la sala número ${partida.codiPartida}.`);
   }
 });
 
